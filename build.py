4ae558b342fa09dd3ff65f49d989fc91636df0e5
David Given <dg@cowlark.com>
2023-09-28 17:03:51 +0200

    Produce a 'binary' distribution.

--- build.py
+++ build.py
@@ -1,3 +1,20 @@
-from build.ab2 import export
+from build.ab2 import export, normalrule
 
-export(name="all", items={}, deps=["tests"])
+normalrule(
+        name="distribution",
+        ins=[
+            "./build/ab.py",
+            "./build/c.py",
+            "./build/pkg.py",
+            "./build/ab.mk"
+        ],
+        outs=[
+            "distribution.tar.xz"
+        ],
+        commands=[
+            "tar cJf {outs[0]} {ins}"
+        ],
+        label="ZIP"
+)
+
+export(name="all", items={"distribution.tar.xz": "+distribution"}, deps=["tests"])
