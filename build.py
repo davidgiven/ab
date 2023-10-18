afa607ded0c2a2c7e24c3ea1e4893a1eeddd820a
David Given <dg@cowlark.com>
2023-10-19 00:33:01 +0200

    Remember to export protobuf.py.

--- build.py
+++ build.py
@@ -2,7 +2,13 @@ from build.ab import export, normalrule
 
 normalrule(
     name="distribution",
-    ins=["./build/ab.py", "./build/c.py", "./build/pkg.py", "./build/ab.mk"],
+    ins=[
+        "./build/ab.py",
+        "./build/c.py",
+        "./build/pkg.py",
+        "./build/ab.mk",
+        "./build/protobuf.py",
+    ],
     outs=["distribution.tar.xz"],
     commands=["tar cJf {outs[0]} {ins}"],
     label="ZIP",
