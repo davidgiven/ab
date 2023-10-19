31482ce035a734f33c4cc76b3ec0d93b156efa84
David Given <dg@cowlark.com>
2023-10-19 23:49:53 +0200

    Don't use .SECONDARY as it seems to provoke pathalogical behaviour in make.

--- build/ab.mk
+++ build/ab.mk
@@ -7,27 +7,9 @@ CFLAGS ?= -g -Og
 LDFLAGS ?= -g
 hide = @
 PKG_CONFIG ?= pkg-config
-export PKG_CONFIG
-
-.SECONDARY:
+ECHO ?= echo
 
 include $(OBJ)/build.mk
-
-#Ifndef AB_LOADED
-## For the first run, when the generated build.mk does not yet exist.
-ECHO = echo
-#else
-## For the second run. This ensures that the submake does not run rules
-## to try and create the build.mk file.
-#ifndef ECHO
-#T := $(shell $(MAKE) $(MAKECMDGOALS) --no-print-directory \
-#      -nrRf $(firstword $(MAKEFILE_LIST)) \
-#      ECHO="COUNTTHIS" | grep -c "COUNTTHIS")
-#N := x
-#C = $(words $N)$(eval N := x $N)
-#ECHO = echo [$C/$T]
-#endif
-#endif
 
 .PHONY: update-ab
 update-ab:
