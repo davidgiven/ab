9e815d4040d819c2d57de3e8c346abff5f4e7183
David Given <dg@cowlark.com>
2024-08-31 20:40:47 +0200

    Remember to test that cheader header definitions are properly included.

--- tests/cxxprogram_compile_test.cc
+++ tests/cxxprogram_compile_test.cc
@@ -4,6 +4,12 @@
 #error CHEADER not defined
 #endif
 
+#include "headers/test.h"
+
+#ifndef HEADER_INCLUDED
+#error HEADER_INCLUDED not defined
+#endif
+
 int main(int argc, const char* argv[])
 {
     std::cout << "Hello, world!\n";
