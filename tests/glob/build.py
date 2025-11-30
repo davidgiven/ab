3c3a9298a5bf1be102de617b1c1982a15a5f1247
David Given <dg@cowlark.com>
2025-12-01 00:14:36 +0100

    Add missing file.

---
+++ tests/glob/build.py
@@ -0,0 +1,28 @@
+from build.ab import getcwd
+from build.utils import glob
+from hamcrest import assert_that, contains_inanyorder
+
+assert_that(
+    glob(["*.py", "*.include", "*.q"], exclude=["*exclude*"]),
+    contains_inanyorder("tests/glob/testfile.include", "tests/glob/build.py"),
+)
+
+assert_that(
+    glob(["**/*.q"]),
+    contains_inanyorder("tests/glob/subdir/testfile.q"),
+)
+
+assert_that(
+    glob(["subdir/*.q"]),
+    contains_inanyorder("tests/glob/subdir/testfile.q"),
+)
+
+assert_that(
+    glob(["*.q"], dir="./subdir"),
+    contains_inanyorder("tests/glob/subdir/testfile.q"),
+)
+
+assert_that(
+    glob(["*.q"], dir="./subdir", relative_to="./subdir"),
+    contains_inanyorder("testfile.q"),
+)
