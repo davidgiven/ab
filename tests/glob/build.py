7bb1b77c0cae9a65ce43e745c3ca70f6ec0af0cc
David Given <dg@cowlark.com>
2025-12-17 20:58:49 +0100

    Fix a bunch of issues with the new glob, including exclude and globs starting with ./

--- tests/glob/build.py
+++ tests/glob/build.py
@@ -3,7 +3,11 @@ from build.utils import glob
 from hamcrest import assert_that, contains_inanyorder
 
 assert_that(
-    glob(["*.py", "*.include", "*.q"], exclude=["*exclude*"]),
+    glob(
+        ["./*.py", "./*.include", "./*.q"],
+        exclude=["**/*exclude*"],
+        dir="tests/glob",
+    ),
     contains_inanyorder("tests/glob/testfile.include", "tests/glob/build.py"),
 )
 
@@ -13,13 +17,13 @@ assert_that(
 )
 
 assert_that(
-    glob(["subdir/*.q"]),
+    glob(["./subdir/*.q"]),
     contains_inanyorder("tests/glob/subdir/testfile.q"),
 )
 
 assert_that(
     glob(["*.q"], dir="./subdir"),
-    contains_inanyorder("tests/glob/subdir/testfile.q"),
+    contains_inanyorder(),
 )
 
 assert_that(
